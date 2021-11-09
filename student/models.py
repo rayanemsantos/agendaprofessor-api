9dc37e9068c1d1639bbdc12306e16f1e91dd18af
Rayane Santos <rayanesantos-131@hotmail.com>
2021-11-09 19:33:04 -0300

    chamada requests

--- student/models.py
+++ student/models.py
@@ -1,3 +1,4 @@
+import requests
 from django.db import models
 from django.utils.translation import gettext as _
 from django.utils import timezone
@@ -7,6 +8,7 @@ class Student(models.Model):
     registration_id = models.PositiveIntegerField(_("matrícula"), null=True, blank=True) 
     responsible_name = models.CharField(_("nome do responsável"), max_length=255, null=True, blank=True) 
     responsible_contact = models.CharField(_("contato do responsável"), max_length=255, null=True, blank=True) 
+    cep = models.CharField(_("cep"), max_length=255, null=True, blank=True) 
     address_street = models.CharField(_("rua"), max_length=255, null=True, blank=True) 
     address_number = models.CharField(_("número"), max_length=255, null=True, blank=True) 
     address_district = models.CharField(_("bairro"), max_length=255, null=True, blank=True)
@@ -20,4 +22,14 @@ class Student(models.Model):
         if not self.creation_datetime:
             self.creation_datetime = timezone.now()
         self.edition_datetime = timezone.now()
+        if self.cep:
+            self.set_address()
         return super(Student, self).save(*args, **kwargs)
+
+    def set_address(self):
+        response = requests.get('http://viacep.com.br/ws/{}/json/'.format(self.cep))
+        print(response.__dict__)
+        if response.status_code == 200 and 'erro' not in response.json():
+            self.address_street = response.json()['logradouro']
+            self.address_district = response.json()['bairro']
+
